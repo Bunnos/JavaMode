817674f205569eae156a04d7e643914bb4920282
ANTIUMBO\antiumbo <boyang.li@jsxfedu.com>
2018-05-24 17:23:26 +0800

    bug fix

--- src/main/java/com/bran/filter/OrCriteria.java
+++ src/main/java/com/bran/filter/OrCriteria.java
@@ -8,17 +8,17 @@ import java.util.List;
  **/
 public class OrCriteria implements Criteria {
     private Criteria criteria;
-    private Criteria otheCriteria;
+    private Criteria otherCriteria;
 
     public OrCriteria(Criteria criteria, Criteria otherCriteria) {
         this.criteria = criteria;
-        this.otheCriteria = otherCriteria;
+        this.otherCriteria = otherCriteria;
     }
 
     @Override
     public List<Person> meetCriteria(List<Person> persons) {
         List<Person> firstCriteriaItems = criteria.meetCriteria(persons);
-        List<Person> otherCriteriaItems = otheCriteria.meetCriteria(persons);
+        List<Person> otherCriteriaItems = otherCriteria.meetCriteria(persons);
         for (Person person : otherCriteriaItems){
             if (!firstCriteriaItems.contains(person)){
                 firstCriteriaItems.add(person);

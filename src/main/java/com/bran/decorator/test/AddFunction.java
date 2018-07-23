25d47cccdd184b80a810e7dc7ae543552feef1b5
ANTIUMBO\antiumbo <boyang.li@jsxfedu.com>
2018-07-23 16:28:18 +0800

    更新装饰者模式

--- src/main/java/com/bran/decorator/test/AddFunction.java
+++ src/main/java/com/bran/decorator/test/AddFunction.java
@@ -5,13 +5,14 @@ package com.bran.decorator.test;
  * 首先装饰者类是抽象者类，方便子类继承扩展
  * 其次要实现要扩展功能的接口
  * 最后在创建装饰者类的时候要把被装饰对象传入
+ *
  * @author antiumbo
  * @date on 2018/5/25
  **/
-public abstract class AddFunction implements IFunction{
-    protected IFunction iFunction;
+public abstract class AddFunction implements IFunction {
+    protected AddFunction addFunction;
 
-    protected AddFunction(IFunction iFunction) {
-        this.iFunction = iFunction;
+    protected AddFunction(AddFunction addFunction) {
+        this.addFunction = addFunction;
     }
 }
